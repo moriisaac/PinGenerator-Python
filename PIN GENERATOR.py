7e7bf814fee5a62d5c173f89b345aa482861f38b
moriisaac <96442087+moriisaac@users.noreply.github.com>
2022-06-19 15:50:08 +0300

    Add files via upload

---
+++ PIN GENERATOR.py
@@ -0,0 +1,7 @@
+import random
+number = int(input("Input Pin Number: "))
+trial = 0
+while(trial != number):
+    trial = random.randint(0, 9999)
+    print(trial)
+print("Your Pin is" + str(number))
